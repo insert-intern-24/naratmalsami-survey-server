3d307d3fbda1b41d7e6e3cc7a18bcdfe802d46fe
GAMZAMANDU <galbimandu1126@gmail.com>
2025-02-10 19:05:53 +0000

    fix : 빈 값 조회 이슈 해결

--- db/model/model.go
+++ db/model/model.go
@@ -2,10 +2,10 @@ package model
 
 // 단어에 대한 평가 수치를 저장하는 테이블
 type WordOfRating struct {
-	ID           uint   `gorm:"primaryKey"`
-	OriginalWord string `gorm:"type:varchar(30);uniqueIndex;not null"`
-	RefinedWord  string `gorm:"type:varchar(30);not null"`
-	Meaning      string `gorm:"type:text"`
-	WholeRating  int    `gorm:"default:0"`
-	Member       int    `gorm:"default:0"`
+	ID           uint   `gorm:"primaryKey" json:"id"`
+	OriginalWord string `gorm:"column:original_word;type:varchar(30);uniqueIndex;not null" json:"original_word"`
+	RefinedWord  string `gorm:"column:refined_word;type:varchar(30);not null" json:"refined_word"`
+	Meaning      string `gorm:"column:meaning;type:text" json:"meaning"`
+	WholeRating  int    `gorm:"column:whole_rating;default:0" json:"whole_rating"`
+	Member       int    `gorm:"column:member;default:0" json:"member"`
 }
--- db/queries.go
+++ db/queries.go
@@ -1,15 +1,14 @@
 package db
 
 import (
-	"log"
-
 	"gorm.io/gorm"
+	"log"
 	"naratmalsami-survey-server/db/model"
 )
 
 func (db *WordDB) GetWordsInRange(startId, endId uint) ([]model.WordOfRating, error) {
 	var words []model.WordOfRating
-	result := db.Where("id BETWEEN ? AND ?", startId, endId).Find(&words)
+	result := db.Where("id BETWEEN ? AND ?", startId, endId).Table("word_of_rating").Find(&words)
 	if result.Error != nil {
 		log.Println("조회 에러: ", result.Error)
 		return nil, result.Error
@@ -20,7 +19,10 @@ func (db *WordDB) GetWordsInRange(startId, endId uint) ([]model.WordOfRating, er
 func (db *WordDB) UpdateRating(ids []uint, ratings []int) error {
 	for i := 0; i < len(ids); i++ {
 		if err := db.Model(&model.WordOfRating{}).Where("id = ?", ids[i]).
-			Update("whole_rating", gorm.Expr("whole_rating + ?", ratings[i])).Error; err != nil {
+			Updates(map[string]interface{}{
+				"whole_rating": gorm.Expr("whole_rating + ?", ratings[i]),
+				"member":       gorm.Expr("member + 1"),
+			}).Error; err != nil {
 			return err
 		}
 	}
