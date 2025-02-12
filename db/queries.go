5d49f1f45017947f433a3f5bc6d9102fc278a64c
gamzamandu <galbimandu1126@gmail.com>
2025-02-12 20:23:31 +0900

    add : CreateUser 쿼리 추가

--- db/queries.go
+++ db/queries.go
@@ -6,9 +6,9 @@ import (
 	"naratmalsami-survey-server/db/model"
 )
 
-func (db *WordDB) GetWordsInRange(startId, endId uint) ([]model.WordOfRating, error) {
-	var words []model.WordOfRating
-	result := db.Where("id BETWEEN ? AND ?", startId, endId).Table("word_of_rating").Find(&words)
+func (db *DataDB) GetWordsInRange(startId, endId uint) ([]model.Words, error) {
+	var words []model.Words
+	result := db.Where("word_id BETWEEN ? AND ?", startId, endId).Table("words").Find(&words)
 	if result.Error != nil {
 		log.Println("조회 에러: ", result.Error)
 		return nil, result.Error
@@ -16,15 +16,24 @@ func (db *WordDB) GetWordsInRange(startId, endId uint) ([]model.WordOfRating, er
 	return words, nil
 }
 
-func (db *WordDB) UpdateRating(ids []uint, ratings []int) error {
+func (db *DataDB) UpdateRating(ids []uint, ratings []int) error {
 	for i := 0; i < len(ids); i++ {
-		if err := db.Model(&model.WordOfRating{}).Where("id = ?", ids[i]).
-			Updates(map[string]interface{}{
-				"whole_rating": gorm.Expr("whole_rating + ?", ratings[i]),
-				"member":       gorm.Expr("member + 1"),
-			}).Error; err != nil {
+		if err := db.Model(&model.Words{}).Where("word_id = ?", ids[i]).
+				Updates(map[string]interface{}{
+					"whole_rating": gorm.Expr("whole_rating + ?", ratings[i]),
+					"member":       gorm.Expr("member + 1"),
+				}).Error; err != nil {
 			return err
 		}
 	}
 	return nil
+}
+
+func (db *DataDB) CreateUser() (string, error) {
+	user := model.Users{}
+	result := db.DB.Create(&user)
+	if result.Error != nil {
+		return "", result.Error
+	}
+	return user.UserId, nil
 }
