1acfa6d49d6c6dcbbad6eebcacd3a66365ae0eec
gamzamandu <galbimandu1126@gmail.com>
2025-02-12 20:22:38 +0900

    chore : word_of_rating 삭제, (words, users, voted) model 추가

--- db/connect.go
+++ db/connect.go
@@ -9,11 +9,11 @@ import (
 	"naratmalsami-survey-server/db/model"
 )
 
-type WordDB struct {
+type DataDB struct {
 	*gorm.DB
 }
 
-func Connect_DB() (*WordDB, error) {
+func Connect_DB() (*DataDB, error) {
 	dsn := os.Getenv("DATABASE_DSN") // 환경 변수에서 DSN 읽기
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -23,10 +23,10 @@ func Connect_DB() (*WordDB, error) {
 	fmt.Println("MySQL에 성공적으로 연결되었습니다!")
 
 	// 모델(table) 마이그레이션
-	err = db.AutoMigrate(&model.WordOfRating{})
+	err = db.AutoMigrate(&model.Words{}, &model.Users{}, &model.Voted{})
 	if err != nil {
 		return nil, err
 	}
 
-	return &WordDB{DB: db}, nil
+	return &DataDB{DB: db}, nil
 }
--- db/model/model.go
+++ db/model/model.go
@@ -1,11 +1,22 @@
 package model
 
-// 단어에 대한 평가 수치를 저장하는 테이블
-type WordOfRating struct {
-	ID           uint   `gorm:"primaryKey" json:"id"`
+// Words 단어 테이블
+type Words struct {
+	WordId       uint   `gorm:"primaryKey" json:"word_id"`
 	OriginalWord string `gorm:"column:original_word;type:varchar(30);uniqueIndex;not null" json:"original_word"`
 	RefinedWord  string `gorm:"column:refined_word;type:varchar(30);not null" json:"refined_word"`
 	Meaning      string `gorm:"column:meaning;type:text" json:"meaning"`
-	WholeRating  int    `gorm:"column:whole_rating;default:0" json:"whole_rating"`
-	Member       int    `gorm:"column:member;default:0" json:"member"`
+}
+
+// Users 유저 테이블
+type Users struct {
+	UserId string `gorm:"type:char(36);primaryKey;default:(UUID())" json:"user_id"`
+}
+
+// Voted 단어 평가 테이블
+type Voted struct {
+	WordId    uint   `gorm:"primaryKey" json:"word_id"`
+	UserId    string `gorm:"primaryKey" json:"user_id"`
+	Rating    int    `gorm:"column:rating;not null" json:"rating"`
+	AtCreated string `gorm:"column:at_created;type:timestamp;not null" json:"at_created"`
 }
